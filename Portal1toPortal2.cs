68795aa89512e9dfc0152bb96f98a5f0b769e500
Artem Burbela <129102839+ArTi4iO@users.noreply.github.com>
2023-06-13 10:16:14 +0200

    Add teleport script (#1)
    * Add files via upload
    
    Added a player teleportation system
    
    * Update README.md
    
    * Add files via upload

---
+++ Portal1toPortal2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Portal1toPortal2 : MonoBehaviour
+{
+    public GameObject portal2;
+
+    private bool isTeleporting = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(StartTeleportation(other.gameObject));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Jeśli gracz wyjdzie z portalu, zatrzymaj teleportację
+            StopCoroutine(StartTeleportation(other.gameObject));
+            isTeleporting = false;
+        }
+    }
+
+    IEnumerator StartTeleportation(GameObject player)
+    {
+        // Sprawdź, czy teleporacja już się odbywa
+        if (isTeleporting) yield break;
+
+        isTeleporting = true;
+
+        // Dodaj wiersz debugowania, aby wyświetlić rozpoczęcie teleporacji
+        Debug.Log("Rozpoczęcie teleportacji");
+
+        yield return new WaitForSeconds(3f);
+
+        player.transform.position = portal2.transform.position;
+
+        // Dodaj wiersz debugowania, aby wyświetlić zakończenie teleporacji
+        Debug.Log("Zakończenie teleportacji");
+
+        isTeleporting = false;
+    }
+}
---
+++ TeleportManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportManager : MonoBehaviour
+{
+    public int[] password = new int[] { 1, 1, 1 }; // Hasło do aktywacji teleportu, można zmienić
+    [SerializeField] private int[] inputPassword = new int[3]; // Służy do identyfikacji w inspektorze Unity
+    public GameObject teleport_1; // Dodać obiekt gry w inspektorze
+    public GameObject teleport_2; // Dodać obiekt gry w inspektorze
+
+    public void AddToInputPassword(int index, int value)
+    {
+        inputPassword[index - 1] = value;
+
+        // Sprawdź, czy tablice password i inputPassword są zgodne
+        bool isMatch = true;
+        for (int i = 0; i < password.Length; i++)
+        {
+            if (password[i] != inputPassword[i])
+            {
+                isMatch = false;
+                break;
+            }
+        }
+
+        // Jeśli tablice są zgodne, wywołaj zdarzenie
+        if (isMatch)
+        {
+            teleport_1.SetActive(true);
+        }
+    }
+}
